c31dad8e1ae834a0d90e82cba12acc7d6c036ff9
mrbobcampbell <30734305+mrbobcampbell@users.noreply.github.com>
2017-08-06 15:11:43 -0400

    updating rectangle

--- class_rectangle.py
+++ class_rectangle.py
@@ -12,6 +12,7 @@ class Rectangle():
     def change_size(self, w, l):
         self.width = w
         self.len = l
+        self.rectangles.append((self.width, self.len))
 
     def __repr__(self):
         return repr(self.len)
@@ -19,9 +20,11 @@ class Rectangle():
 
 rectangle1 = Rectangle(10, 20)
 rectangle2 = Rectangle(200, 200)
+print(Rectangle.rectangles)
 print(rectangle1.area())
 rectangle1.change_size(20, 40)
 print(rectangle1.area())
+
 print(Rectangle.rectangles)
 print(rectangle2.area())
 print(rectangle1, "<-- this is it")
